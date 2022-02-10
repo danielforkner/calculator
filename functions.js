e190d5815ad2fc5bfdcb36dd6695b8a003315d8c
danielforkner <daniel.forkner@gmail.com>
2022-02-09 20:37:31 -0500

    added history

--- functions.js
+++ functions.js
@@ -4,6 +4,8 @@ let inputArr = [];
 let input = '';
 let result = 0;
 let display = `0`;
+let isDot = false; // does not allow a '.' if there already is one
+let isResult = false; // does not allow modifying (e.g. DEL) a returned result
 
 // operators
 const add = (a, b) => a + b; 
@@ -25,110 +27,146 @@ const operate = function() {
                 return divide(acc, el);
         }
     });
-    display = `${result}`
+    // update history, update display, and reset variables 
+    updateHistory(display, result);
+    display = `${result}`;
     input = result;
     inputArr = [];
+    isDot = false;
+    isResult = true;
 };
 
-// calculator functions
 const clear = () => {
     inputArr = [];
     functions = [];
     display = '0';
     input = '0';
+    isDot = false;
+    isResult = false;
+};
+
+updateHistory = function(display, result) {
+    let historyContainer = document.querySelector('.historyContainer');
+    let historyDiv = document.createElement('div')
+    historyDiv.innerText = `${display} = ${result}`;
+    historyContainer.append(historyDiv);
 }
 
 // User Interface
 const buttons = document.querySelectorAll('.calcBtn');
 const displayBox = document.querySelector('.display');
 
-
 buttons.forEach(btn => {
-    btn.addEventListener('click', (e) => clickButton(e.target.id));
+    btn.addEventListener('click', (e) => clickButton(e));
 });
 
-const clickButton = function(id) {
-    if (display === '0') {display = ''}
-    switch (id) {
-        // Numbers
-        case 'btn1':
-            display = display.concat('1');
-            input = input.concat('1');
-            break;
-        case 'btn2':
-            display = display.concat('2');
-            input = input.concat('2');
-            break;
-        case 'btn3':
-            display = display.concat('3');
-            input = input.concat('3');
-            break;
-        case 'btn4':
-            display = display.concat('4');
-            input = input.concat('4');
-            break;
-        case 'btn5':
-            display = display.concat('5');
-            input = input.concat('5');
-            break;
-        case 'btn6':
-            display = display.concat('6');
-            input = input.concat('6');
-            break;
-        case 'btn7':
-            display = display.concat('7');
-            input = input.concat('7');
-            break;
-        case 'btn8':
-            display = display.concat('8');
-            input = input.concat('8');
-            break;
-        case 'btn9':
-            display = display.concat('9');
-            input = input.concat('9');
-            break;
+const clickButton = function(btn) {
+    if (display === '0') {display = ''};
+    if (btn.target.classList.contains('num') && !isResult) {
+        switch (btn.target.id) {
+            // Numbers
+            case 'btn1':
+                display = display.concat('1');
+                input = input.concat('1');
+                break;
+            case 'btn2':
+                display = display.concat('2');
+                input = input.concat('2');
+                break;
+            case 'btn3':
+                display = display.concat('3');
+                input = input.concat('3');
+                break;
+            case 'btn4':
+                display = display.concat('4');
+                input = input.concat('4');
+                break;
+            case 'btn5':
+                display = display.concat('5');
+                input = input.concat('5');
+                break;
+            case 'btn6':
+                display = display.concat('6');
+                input = input.concat('6');
+                break;
+            case 'btn7':
+                display = display.concat('7');
+                input = input.concat('7');
+                break;
+            case 'btn8':
+                display = display.concat('8');
+                input = input.concat('8');
+                break;
+            case 'btn9':
+                display = display.concat('9');
+                input = input.concat('9');
+                break;
+            case 'btn0':
+                display = display.concat('0');
+                input = input.concat('0');
+                break;
+            case 'btnDot':
+                if (isDot) {break;}
+                display = display.concat('.');
+                input = input.concat('.');
+                isDot = true;
+                break;
+        };
+    }
         // Calculator functions
-        case 'btnAdd':
-            functions.push('add');    
-            display = display.concat(' + ');
-            inputArr.push(parseFloat(input));
-            input = '';
-            break;
-        case 'btnSubtract':
-            functions.push('subtract');    
-            display = display.concat(' - ');
-            inputArr.push(parseFloat(input));
-            input = '';
-            break;
-        case 'btnMultiply':
-            functions.push('multiply');    
-            display = display.concat(' * ');
-            inputArr.push(parseFloat(input));
-            input = '';
-            break;
-        case 'btnDivide':
-            functions.push('divide');    
-            display = display.concat(' / ');
-            inputArr.push(parseFloat(input));
-            input = '';
-            break;
-        case 'btnOperate':
-            inputArr.push(parseFloat(input));
-            operate();
-            break;
-        case 'btnDelete':
-            if (display.length === 1 || display.length === 0) {
-                display = '0'
-            } else if (display[display.length - 1] === ' ') {
-                return;
-            } else {
-                display = display.substring(0, display.length - 1);
-            }
-            break;
-        case 'btnClear':
-            clear();
-            break;
-            
+    else {
+        switch (btn.target.id) {
+            case 'btnAdd':
+                functions.push('add');    
+                display = display.concat(' + ');
+                inputArr.push(parseFloat(input));
+                input = '';
+                isDot = false;
+                isResult = false;
+                break;
+            case 'btnSubtract':
+                functions.push('subtract');    
+                display = display.concat(' - ');
+                inputArr.push(parseFloat(input));
+                input = '';
+                isDot = false;
+                isResult = false;
+                break;
+            case 'btnMultiply':
+                functions.push('multiply');    
+                display = display.concat(' * ');
+                inputArr.push(parseFloat(input));
+                input = '';
+                isDot = false;
+                isResult = false;
+                break;
+            case 'btnDivide':
+                functions.push('divide');    
+                display = display.concat(' / ');
+                inputArr.push(parseFloat(input));
+                input = '';
+                isDot = false;
+                isResult = false;
+                break;
+            case 'btnOperate':
+                inputArr.push(parseFloat(input));
+                operate();
+                break;
+            case 'btnDelete':
+                if (isResult) {
+                    return;
+                } else if (display.length === 1 || display.length === 0) {
+                    display = '0'
+                } else if (display[display.length - 1] === ' ') {
+                    return;
+                } else {
+                    display = display.substring(0, display.length - 1);
+                }
+                break;
+            case 'btnClear':
+                clear();
+                break;     
+        };
     };
     displayBox.innerText = display;
 } 
